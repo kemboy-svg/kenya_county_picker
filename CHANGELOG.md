2b4597554f0ae988ce41fde5f4c43c9a783c404c
kemboy-svg <eliaskemboy645@gmail.com>
2024-09-20 16:28:15 +0300

    search field

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 ## 0.0.1
+- Initial Open Source release.
 
-* TODO: Describe initial release.
+## 0.0.2
+### Added
+- `Search field` on the county list  `KenyaCountyPicker`.
--- lib/src/kenya_county_picker.dart
+++ lib/src/kenya_county_picker.dart
@@ -14,26 +14,108 @@ class KenyaCountyPicker extends StatefulWidget {
 
 class _KenyaCountyPickerState extends State<KenyaCountyPicker> {
   String? selectedCounty;
+  TextEditingController _searchController = TextEditingController();
+  List<County> filteredCounties = counties; 
+  bool isDropdownOpen = false;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField<String>(
-      hint: Text('Select a County'),
-      value: selectedCounty,
-      onChanged: (String? newValue) {
-        setState(() {
-          selectedCounty = newValue!;
-        });
-        widget.onCountySelected!(newValue!);
-      },
-      items: counties.map<DropdownMenuItem<String>>((String county) {
-        return DropdownMenuItem<String>(
-          value: county,
-          child: Text(county),
-        );
-      }).toList(),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: _toggleDropdown, 
+            child: InputDecorator(
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                label: selectedCounty!=null ? const Text('county'): const Text("select county") ,
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 10, horizontal: 12),              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(selectedCounty ?? ''),
+                  Icon(Icons.arrow_drop_down),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          if (isDropdownOpen)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _searchController,
+                    decoration: const InputDecoration(
+                      labelText: 'Search County',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                    ),
+                    onChanged: _filterCounties,
+                  ),
+                  const SizedBox(height: 10),
+                  SingleChildScrollView(
+                    child: Container(
+                      height: 400,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: ListView.builder(
+                        itemCount: filteredCounties.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(filteredCounties[index].name), 
+                            onTap: () => _selectCounty(filteredCounties[index].name), 
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
     );
   }
+
+  void _filterCounties(String query) {
+    setState(() {
+      filteredCounties = counties
+          .where((county) => county.name
+              .toLowerCase()
+              .contains(query.toLowerCase())) 
+          .toList();
+    });
+  }
+
+  void _selectCounty(String county) {
+    setState(() {
+      selectedCounty = county;
+      isDropdownOpen = false; 
+      _searchController.clear(); 
+      filteredCounties = counties; 
+    });
+    if (widget.onCountySelected != null) {
+      widget.onCountySelected!(county);
+    }
+  }
+
+  void _toggleDropdown() {
+    setState(() {
+      isDropdownOpen = !isDropdownOpen;
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 }
-
-
